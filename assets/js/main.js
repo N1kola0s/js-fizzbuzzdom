47d9e0688fc8fe619dfa5c5f4c5bc4fceca506b8
NicolaMigliore <auster.mig@gmail.com>
2022-03-28 17:44:45 +0200

    add correction in if structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,26 +3,28 @@ let numb = document.getElementById("numbers");
 for (let i=1; i<=100; i++){
 console.log(i);
 
-if(i % 3 == 0){
+if(i % 15 == 0){
+
+    let mult_fifteen = `<div class="numb  bg_red">fizzbuzz</div>`
+
+    numb.insertAdjacentHTML(`beforeend`, mult_fifteen); 
+
+}
+
+else if (i % 3 == 0) {
 
     let mult_three = `<div class="numb bg_green">fizz</div>`;
 
     numb.insertAdjacentHTML(`beforeend`, mult_three); 
-    
-}
 
-else if (i % 5 == 0) {
+} 
+
+else if (i % 5 == 0){
 
     let mult_five = `<div class="numb bg_yellow">buzz</div>`;
 
     numb.insertAdjacentHTML(`beforeend`, mult_five); 
-
-} 
-
-else if (i % 15 == 0){
-    let mult_fifteen = `<div class="numb  bg_red">fizzbuzz</div>`
-
-    numb.insertAdjacentHTML(`beforeend`, mult_fifteen); 
+    
 }
 
 else {
